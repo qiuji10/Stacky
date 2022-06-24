4299d3f42fee3c47a501cf8f08905e95406254b5
qiuji <74701224+qiuji10@users.noreply.github.com>
2022-06-25 05:45:31 +0800

    Redo Whole Thing

---
+++ Assets/1_Scripts/CubeBehaviour.cs
@@ -0,0 +1,168 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CubeBehaviour : MonoBehaviour
+{
+    [SerializeField] private float speed;
+    [SerializeField] private bool isX;
+
+    public Transform prevCube;
+    public Vector3 pointA, pointB;
+
+    private GameManager gm;
+    private bool stop, isLeft;
+
+    private void Awake()
+    {
+        gm = FindObjectOfType<GameManager>();
+    }
+
+    void Update()
+    {
+        if (Input.GetMouseButtonDown(0))
+            stop = true;
+
+        if (!stop)
+        {
+            Moving();
+        }
+        else
+        {
+            speed = 0;
+            if (isX)
+            {
+                float distance = CalDistance(transform.position.x, prevCube.position.x);
+                float newXSize =  transform.localScale.x - distance;
+
+                transform.localScale = new Vector3(newXSize, 1, transform.localScale.z);
+
+                //here should add combo, and if distance  < 0.5 also counted as 0
+                if (distance == 0)
+                    transform.position = new Vector3(transform.position.x, transform.position.y, transform.position.z);
+                else
+                    transform.position = new Vector3((transform.position.x + prevCube.position.x) / 2, transform.position.y, transform.position.z);
+
+                SpawnNewCube(gm.yPrefab);
+            }
+            else
+            {
+                float distance = CalDistance(transform.position.z, prevCube.position.z);
+                float newXSize = transform.localScale.x - distance;
+
+                transform.localScale = new Vector3(newXSize, 1, transform.localScale.z);
+
+                if (distance == 0)
+                    transform.position = new Vector3(transform.position.x, transform.position.y, transform.position.z);
+                else
+                    transform.position = new Vector3(transform.position.x, transform.position.y, (transform.position.z + prevCube.position.z) / 2);
+
+                SpawnNewCube(gm.xPrefab);
+            }
+            enabled = false;
+        }
+    }
+
+    private void Moving()
+    {
+        if (isX)
+        {
+            if (isLeft)
+            {
+                transform.Translate(Vector3.left * speed * Time.deltaTime);
+                if (transform.position.x < pointA.x)
+                {
+                    isLeft = false;
+                }
+            }
+            else
+            {
+                transform.Translate(Vector3.right * speed * Time.deltaTime);
+                if (transform.position.x > pointB.x)
+                {
+                    isLeft = true;
+                }
+            }
+        }
+        else
+        {
+            if (isLeft)
+            {
+                transform.Translate(Vector3.left * speed * Time.deltaTime);
+                if (transform.position.z > pointA.z)
+                {
+                    isLeft = false;
+                }
+            }
+            else
+            {
+                transform.Translate(Vector3.right * speed * Time.deltaTime);
+                if (transform.position.z < pointB.z)
+                {
+                    isLeft = true;
+                }
+            }
+        }
+    }
+
+    /// <summary>
+    /// Calculate real distance, 
+    /// a is current cube x, b is previous cube x
+    /// </summary>
+    /// <param name="a"></param>
+    /// <param name="b"></param>
+    /// <returns></returns>
+    private float CalDistance(float a, float b)
+    {
+        if (a >= 0 && b >= 0)
+        {
+            if (a > b)
+            {
+                Debug.Log("1a");
+                return a - b;
+            }
+            else
+            {
+                Debug.Log("1b");
+                return b - a;
+            }
+        }
+        else if (a < 0 && b > 0)
+        {
+            Debug.Log("2");
+            return Mathf.Abs(a) - b;
+        }
+        else if (a < 0 && b <= 0)
+        {
+            
+            if (a < b)
+            {
+                Debug.Log("3a");
+                return Mathf.Abs(a) + b;
+            }
+            else
+            {
+                Debug.Log("3b");
+                return Mathf.Abs(b) + a;
+            }
+        }
+        else
+        {
+            Debug.Log("4");
+            return 0;
+        }
+    }
+
+    private void SpawnNewCube(GameObject prefabCube)
+    {
+        GameObject cube = Instantiate(prefabCube, transform.parent);
+        cube.transform.localScale = new Vector3(transform.localScale.z, transform.localScale.y, transform.localScale.x);
+
+        if (isX)
+            cube.transform.position = new Vector3(transform.position.x, transform.position.y + 1, cube.transform.position.z);
+        else
+            cube.transform.position = new Vector3(cube.transform.position.x, transform.position.y + 1, transform.position.z);
+
+        cube.GetComponent<CubeBehaviour>().prevCube = transform;
+    }
+}
